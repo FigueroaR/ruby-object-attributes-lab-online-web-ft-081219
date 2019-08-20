28b4467502b843f1b53e282e12869633b5f6110e
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-20 20:13:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog 
-  def initialize(name)
-    @name = name
+ def name=(dog_name)
+    @this_dogs_name  = dog_name
   end
- 
   def name
-    @name
-  end
- 
-  def name=(new_name)
-    @name = new_name
-  end
+    @this_dogs_name
+  end 
 end
